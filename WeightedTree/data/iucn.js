b19f06276f05628ef270a95a8ed348d7e04aa2fb
Uwe Helm <uwe.helm@gmail.com>
2021-12-03 14:25:08 -0800

    WeightedTree: initial commit

---
+++ WeightedTree/data/iucn.js
@@ -0,0 +1,488 @@
+var data = [
+  {
+    "Category": "Vertebrates",
+    "values": [
+      {
+        "Category": "Mammals",
+        "values": [     
+              {
+                "Category": "Estimated",
+                "Level1": "Vertebrates",
+                "Level2": "Mammals",
+                "Level3": "Estimated",
+                "Total": "6554",
+              },
+              {
+                "Category": "Evaluated",
+                "Level1": "Vertebrates",
+                "Level2": "Mammals",
+                "Level3": "Evaluated",
+                "Total": "5954",
+              },
+        ],
+//        "TotalEvaluated": 5954,
+//        "TotalEvaluated": 5954,
+      },
+      {
+        "Category": "Birds",
+        "values": [     
+              {
+                "Category": "Estimated",
+                "Level1": "Vertebrates",
+                "Level2": "Birds",
+                "Level3": "Estimated",
+                "Total": "11158",
+              },
+              {
+                "Category": "Evaluated",
+                "Level1": "Vertebrates",
+                "Level2": "Birds",
+                "Level3": "Evaluated",
+                "Total": "11158",
+              },
+        ],
+        "TotalEvaluated": 11158
+      },
+      {
+        "key": "Reptiles",
+        "values": [     
+              {
+                "Category": "Estimated",
+                "Level1": "Vertebrates",
+                "Level2": "Reptiles",
+                "Level3": "Estimated",
+                "Total": "11570",
+              },
+              {
+                "Category": "Evaluated",
+                "Level1": "Vertebrates",
+                "Level2": "Reptiles",
+                "Level3": "Evaluated",
+                "Total": "9132",
+              },
+        ],
+        "TotalEvaluated": 9132
+      },
+      {
+        "key": "Amphibians",
+        "values": [     
+              {
+                "Category": "Estimated",
+                "Level1": "Vertebrates",
+                "Level2": "Mammals",
+                "Level3": "Estimated",
+                "Total": "8361",
+              },
+              {
+                "Category": "Evaluated",
+                "Level1": "Vertebrates",
+                "Level2": "Mammals",
+                "Level3": "Evaluated",
+                "Total": "7215",
+              },
+        ],
+        "TotalEvaluated": 7215
+      },
+      {
+        "key": "Fishes",
+        "values": [     
+              {
+                "Category": "Estimated",
+                "Level1": "Vertebrates",
+                "Level2": "Fishes",
+                "Level3": "Estimated",
+                "Total": "35934",
+              },
+              {
+                "Category": "Evaluated",
+                "Level1": "Vertebrates",
+                "Level2": "Fishes",
+                "Level3": "Evaluated",
+                "Total": "22349",
+              },
+        ],
+        "TotalEvaluated": 22349,
+      },
+    ],
+    "TotalEvaluated": 55808
+  },
+  {
+    "key": "INVERTEBRATES",
+    "values": [
+      {
+        "key": "Insects",
+        "values": [     
+              {
+                "Category": "Estimated",
+                "Level1": "INVERTEBRATES",
+                "Level2": "Insects",
+                "Level3": "Estimated",
+                "Total": "1053578",
+              },
+              {
+                "Category": "Evaluated",
+                "Level1": "INVERTEBRATES",
+                "Level2": "Insects",
+                "Level3": "Evaluated",
+                "Total": "11480",
+              },
+
+        ],
+        "TotalEvaluated": 11480,
+      },
+      {
+        "key": "Molluscs",
+        "values": [     
+              {
+                "Category": "Estimated",
+                "Level1": "INVERTEBRATES",
+                "Level2": "Molluscs",
+                "Level3": "Estimated",
+                "Total": "83125",
+              },
+              {
+                "Category": "Evaluated",
+                "Level1": "INVERTEBRATES",
+                "Level2": "Molluscs",
+                "Level3": "Evaluated",
+                "Total": "8934",
+              },
+        ],
+        "TotalEvaluated": 8934
+      },
+      {
+        "key": "Crustaceans",
+        "values": [     
+              {
+                "Category": "Estimated",
+                "Level1": "INVERTEBRATES",
+                "Level2": "Crustaceans",
+                "Level3": "Estimated",
+                "Total": "80604",
+              },
+              {
+                "Category": "Evaluated",
+                "Level1": "INVERTEBRATES",
+                "Level2": "Crustaceans",
+                "Level3": "Evaluated",
+                "Total": "3189",
+              },
+
+        ],
+        "TotalEvaluated": 3189,
+      },
+      {
+        "key": "Corals",
+        "values": [     
+              {
+                "Category": "Estimated",
+                "Level1": "INVERTEBRATES",
+                "Level2": "Corals",
+                "Level3": "Estimated",
+                "Total": "5351",
+              },
+              {
+                "Category": "Evaluated",
+                "Level1": "INVERTEBRATES",
+                "Level2": "Corals",
+                "Level3": "Evaluated",
+                "Total": "863",
+              },
+
+        ],
+        "TotalEvaluated": 863,
+      },
+      {
+        "key": "Arachnids",
+        "values": [     
+              {
+                "Category": "Estimated",
+                "Level1": "INVERTEBRATES",
+                "Level2": "Arachnids",
+                "Level3": "Estimated",
+                "Total": "110615",
+              },
+              {
+                "Category": "Evaluated",
+                "Level1": "INVERTEBRATES",
+                "Level2": "Arachnids",
+                "Level3": "Evaluated",
+                "Total": "393",
+              },
+
+        ],
+        "TotalEvaluated": 393,
+      },
+      {
+        "key": "Velvet",
+        "values": [     
+              {
+                "Category": "Estimated",
+                "Level1": "INVERTEBRATES",
+                "Level2": "Velvet",
+                "Level3": "Estimated",
+                "Total": "228",
+              },
+              {
+                "Category": "Evaluated",
+                "Level1": "INVERTEBRATES",
+                "Level2": "Velvet",
+                "Level3": "Evaluated",
+                "Total": "11",
+              },
+
+        ],
+        "TotalEvaluated": 11,
+      },
+      {
+        "key": "Horseshoe",
+        "values": [     
+              {
+                "Category": "Estimated",
+                "Level1": "INVERTEBRATES",
+                "Level2": "Horseshoe",
+                "Level3": "Estimated",
+                "Total": "4",
+              },
+              {
+                "Category": "Evaluated",
+                "Level1": "INVERTEBRATES",
+                "Level2": "Horseshoe",
+                "Level3": "Evaluated",
+                "Total": "4",
+              },
+
+        ],
+        "TotalEvaluated": 4,
+      },
+      {
+        "key": "Others",
+        "values": [     
+              {
+                "Category": "Estimated",
+                "Level1": "INVERTEBRATES",
+                "Level2": "Others",
+                "Level3": "Estimated",
+                "Total": "157543",
+              },
+              {
+                "Category": "Evaluated",
+                "Level1": "INVERTEBRATES",
+                "Level2": "Others",
+                "Level3": "Evaluated",
+                "Total": "887",
+              },
+
+        ],
+        "TotalEvaluated": 887,
+      },
+    ],
+    "TotalEvaluated": 25761
+  },
+  {
+    "key": "PLANTS",
+    "values": [
+      {
+        "key": "Mosses",
+        "values": [     
+              {
+                "Category": "Estimated",
+                "Level1": "PLANTS",
+                "Level2": "Mosses",
+                "Level3": "Estimated",
+                "Total": "21925",
+              },
+              {
+                "Category": "Evaluated",
+                "Level1": "PLANTS",
+                "Level2": "Mosses",
+                "Level3": "Evaluated",
+                "Total": "282",
+              },
+
+        ],
+        "TotalEvaluated": 282,
+      },
+      {
+        "key": "Ferns",
+        "values": [     
+              {
+                "Category": "Estimated",
+                "Level1": "PLANTS",
+                "Level2": "Ferns",
+                "Level3": "Estimated",
+                "Total": "11800",
+              },
+              {
+                "Category": "Evaluated",
+                "Level1": "PLANTS",
+                "Level2": "Ferns",
+                "Level3": "Evaluated",
+                "Total": "728",
+              },
+
+        ],
+        "TotalEvaluated": 728,
+      },
+      {
+        "key": "Gymnosperms",
+        "values": [     
+              {
+                "Category": "Estimated",
+                "Level1": "PLANTS",
+                "Level2": "Gymnosperms",
+                "Level3": "Estimated",
+                "Total": "1113",
+              },
+              {
+                "Category": "Evaluated",
+                "Level1": "PLANTS",
+                "Level2": "Gymnosperms",
+                "Level3": "Evaluated",
+                "Total": "1016",
+              },
+
+        ],
+        "TotalEvaluated": 1016,
+      },
+      {
+        "key": "Flowering",
+        "values": [     
+              {
+                "Category": "Estimated",
+                "Level1": "PLANTS",
+                "Level2": "Flowering",
+                "Level3": "Estimated",
+                "Total": "369000",
+              },
+              {
+                "Category": "Evaluated",
+                "Level1": "PLANTS",
+                "Level2": "Flowering",
+                "Level3": "Evaluated",
+                "Total": "54145",
+              },
+
+        ],
+        "TotalEvaluated": 54145,
+      },
+      {
+        "key": "Green",
+        "values": [     
+              {
+                "Category": "Estimated",
+                "Level1": "PLANTS",
+                "Level2": "Green",
+                "Level3": "Estimated",
+                "Total": "11616",
+              },
+              {
+                "Category": "Evaluated",
+                "Level1": "PLANTS",
+                "Level2": "Green",
+                "Level3": "Evaluated",
+                "Total": "16",
+              },
+
+        ],
+        "TotalEvaluated": 16,
+      },
+      {
+        "key": "Red",
+        "values": [     
+              {
+                "Category": "Estimated",
+                "Level1": "PLANTS",
+                "Level2": "Red",
+                "Level3": "Estimated",
+                "Total": "7291",
+              },
+              {
+                "Category": "Evaluated",
+                "Level1": "PLANTS",
+                "Level2": "Red",
+                "Level3": "Evaluated",
+                "Total": "58",
+              },
+
+        ],
+        "TotalEvaluated": 58,
+      },
+      
+      {
+      }
+    ],
+    "TotalEvaluated": 56245
+  },
+  {
+    "key": "FUNGI & PROTISTS",
+    "values": [
+      {
+        "key": "Lichens",
+        "values": [     
+              {
+                "Category": "Estimated",
+                "Level1": "INVERTEBRATES",
+                "Level2": "Insects",
+                "Level3": "Estimated",
+                "Total": "17000",
+              },
+              {
+                "Category": "Evaluated",
+                "Level1": "INVERTEBRATES",
+                "Level2": "Insects",
+                "Level3": "Evaluated",
+                "Total": "76",
+              },
+
+        ],
+        "TotalEvaluated": 76,
+      },
+      {
+        "key": "Mushrooms",
+        "values": [     
+              {
+                "Category": "Estimated",
+                "Level1": "INVERTEBRATES",
+                "Level2": "Insects",
+                "Level3": "Estimated",
+                "Total": "120000",
+              },
+              {
+                "Category": "Evaluated",
+                "Level1": "INVERTEBRATES",
+                "Level2": "Insects",
+                "Level3": "Evaluated",
+                "Total": "469",
+              },
+
+        ],
+        "TotalEvaluated": 469,
+      },
+      {
+        "key": "Brown",
+        "values": [     
+              {
+                "Category": "Estimated",
+                "Level1": "INVERTEBRATES",
+                "Level2": "Insects",
+                "Level3": "Estimated",
+                "Total": "4317",
+              },
+              {
+                "Category": "Evaluated",
+                "Level1": "INVERTEBRATES",
+                "Level2": "Insects",
+                "Level3": "Evaluated",
+                "Total": "15",
+              },
+
+        ],
+        "TotalEvaluated": 15,
+      },
+      {
+      }
+    ],
+    "TotalEvaluated": 560
+  }
+]
